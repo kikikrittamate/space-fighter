dbaa64cf14dc39ada90e02cac5b76264e17b4cb8
kikikrittamate <kikikrittamate@gmail.com>
2021-04-08 22:10:58 +0700

    spacefighter

--- gamelib.py
+++ gamelib.py
@@ -114,6 +114,31 @@ class GameApp(ttk.Frame):
 
         self.parent.bind('<KeyPress>', self.on_key_pressed)
         self.parent.bind('<KeyRelease>', self.on_key_released)
+
+        self.key_pressed_handler = KeyboardHandler()
+        self.key_released_handler = KeyboardHandler()
+
+        self.init_game()
+
+    def animate(self):
+        if not self.is_stopped:
+            self.pre_update()
+
+            remaining_elements = []
+            for element in self.elements:
+                element.update()
+                element.render()
+
+                if element.to_be_deleted:
+                    element.delete()
+                else:
+                    remaining_elements.append(element)
+
+            self.elements = remaining_elements
+
+            self.post_update()
+
+        self.after(self.update_delay, self.animate)
         
     def create_canvas(self):
         self.canvas = tk.Canvas(self, borderwidth=0,
@@ -161,7 +186,17 @@ class GameApp(ttk.Frame):
         pass
 
     def on_key_pressed(self, event):
-        pass
+        self.key_pressed_handler.handle(event)
 
     def on_key_released(self, event):
-        pass
+        self.key_released_handler.handle(event)
+
+
+
+class KeyboardHandler:
+    def __init__(self, successor=None):
+        self.successor = successor
+
+    def handle(self, event):
+        if self.successor:
+            self.successor.handle(event)
